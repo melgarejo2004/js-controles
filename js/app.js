d368a5c1d03023ab83aa252b5eaeea0de032d59b
daniduarte684@gmail.com <melgarejo2004>
2024-07-22 19:52:49 -0400

    mi quinto commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+/*
+    DOMContentLoaded sirve para controlar
+    que primero se cargue TODO el documento
+    HTML, por ultimo javascript
+*/
+documento
+.addEventListener("DOMContentLoaded", función(){
+    alert("Holis");
+});
+
+
+function hola(nombre, apellido) {
+    consola.log(nombre, apellido);
+    //Código Reutilizable
+
+
+}

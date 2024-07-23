0d9189b2ffc7f90ab60f015731d34c00911ce8a3
daniduarte684@gmail.com <melgarejo2004>
2024-07-22 20:27:20 -0400

    modificacion de commit

--- js/app.js
+++ js/app.js
@@ -3,11 +3,23 @@
     que primero se cargue TODO el documento
     HTML, por ultimo javascript
 */
-documento
-.addEventListener("DOMContentLoaded", función(){
-    alert("Holis");
+documento.addEventListener("DOMContentLoaded", función(){
+    //Esta es zona segura
+    //console.log(document.getElementById("txtNombre").value);
+    //console.log(document.getElementById("txtApellido").value);
 });
 
+//añadiendo evento clic al botón mostrar
+const btnMostrar = documento.getElementById("btnMostrar");
+    btnMostrar.addEventListener("clic", function() {
+    //console.log('haciendo clic');3
+    //alert("hice clic");
+    //recuperando info del formulario
+    const nombres = documento.getElementById("txtNombre").valor;
+    const apellido = documento.getElementById("txtApellido").valor;
+    const cedula = documento.getElementById("txtCedula").valor;
+    consola.log('nombres ' +nombres+ ', apellidos ' +apellido+ ', cédula ' + cédula);
+});
 
 function hola(nombre, apellido) {
     consola.log(nombre, apellido);
